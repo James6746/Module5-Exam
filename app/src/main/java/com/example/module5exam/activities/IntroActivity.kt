54fadc3b21b8837d68d600d9c68276b65f7f16e9
James <sobirovjamshid18@gmail.com>
2022-02-25 09:11:09 +0500

    first commit

---
+++ app/src/main/java/com/example/module5exam/activities/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.module5exam.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.module5exam.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
